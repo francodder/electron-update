824ae540a01fc6f377fd18a6e381f66366f0d0d1
franRicoso <frocchia@encodesa.com.ar>
2022-06-01 15:15:31 -0300

    v0.1.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 
 const path = require("path");
 const { app, screen, BrowserWindow, ipcMain, Menu } = require("electron");
-const {autoUpdater} = require("electron-updater");
+const log = require("electron-log");
+const { autoUpdater } = require("electron-updater");
 
 const {
   notificationsListeners,
@@ -11,74 +12,81 @@ const WindowState = require("./utils/WindowState");
 const windowListeners = require("./electronContext/windowListeners");
 
 autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
 
-let template = []
-if (process.platform === 'darwin') {
+let template = [];
+if (process.platform === "darwin") {
   // OS X
   const name = app.getName();
   template.unshift({
     label: name,
     submenu: [
       {
-        label: 'About ' + name,
-        role: 'about'
+        label: "About " + name,
+        role: "about",
       },
       {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() { app.quit(); }
+        label: "Quit",
+        accelerator: "Command+Q",
+        click() {
+          app.quit();
+        },
       },
-    ]
-  })
+    ],
+  });
 }
 
 let winUpdater;
 
 function sendStatusToWindow(text) {
   log.info(text);
-  winUpdater.webContents.send('message', text);
+  winUpdater.webContents.send("message", text);
 }
 function createDefaultWindow() {
   winUpdater = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
   winUpdater.webContents.openDevTools();
-  winUpdater.on('closed', () => {
+  winUpdater.on("closed", () => {
     winUpdater = null;
   });
   winUpdater.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
   return winUpdater;
 }
 
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
   sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
 });
 
 app.on("ready", () => {
-
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
